51d4704dd57d92b37d0749cabf357a84918a9270
Arthur LE MOIGNE <arthur.lemoigne@gmail.com>
2022-08-23 08:38:30 +0200

    Add python helper

--- python/pyarraypool/__init__.py
+++ python/pyarraypool/__init__.py
@@ -1,6 +1,123 @@
-from .pyarraypool import *
+from contextlib import contextmanager
+import logging
+from pathlib import Path
+import tempfile
+from typing import Any, Iterator, Optional, Tuple, Union
+import weakref
+from .pyarraypool import ShmObjectPool
+import numpy as np
+import numpy.typing as npt
+
+MemorySizeType = Union[str, int]
+
+LOGGER = logging.getLogger(__name__)
+
+_GLOBAL_POOL: Optional[ShmObjectPool] = None
 
 
-__doc__ = pyarraypool.__doc__
-if hasattr(pyarraypool, "__all__"):
-	__all__ = pyarraypool.__all__
+class PoolAlreadyExists(Exception):
+    """Pool already exists."""
+
+
+class PoolNotRunning(Exception):
+    """Pool not running."""
+
+
+def _parse_datasize_to_bytes(value: MemorySizeType) -> int:
+    def ensure_positive_value(x: object) -> int:
+        assert isinstance(x, (float, int))
+        if int(x) <= 0:
+            raise ValueError("pool size cannot be null or less", x)
+        return int(x)
+
+    if isinstance(value, str):
+        if value.endswith("G"):
+            return ensure_positive_value(float(value[:-1]) * (1024 ** 3))
+        elif value.endswith("M"):
+            return ensure_positive_value(float(value[:-1]) * (1024 ** 2))
+        elif value.endswith("K"):
+            return ensure_positive_value(float(value[:-1]) * 1024)
+        else:
+            return ensure_positive_value(float(value))
+
+    return ensure_positive_value(value)
+
+
+class ndarrayproxy(np.ndarray):
+    python_id: int = 0
+
+    def __reduce__(self) -> Tuple[Any, ...]:
+        # Check: https://docs.python.org/3/library/pickle.html#object.__reduce__
+
+        return (
+            # Builder
+            ndarrayproxy._shm_reconstruct,
+            # Builder args
+            (self.shape, self.dtype, self.python_id),
+        )
+
+    def __getstate__(self):
+        raise NotImplementedError()
+
+    def __setstate__(self, state) -> None:
+        raise NotImplementedError()
+
+    @classmethod
+    def _shm_reconstruct(cls, shape, dtype, python_id) -> "ndarrayproxy":
+        pool = get_reusable_pool()
+        memview = pool.attach_object(python_id)
+
+        out = cls(shape, dtype=dtype, buffer=memview)
+
+        weakref.finalize(out, pool.detach_object, python_id)
+        return out
+
+
+def make_transferable(arr: np.ndarray) -> ndarrayproxy:
+    python_id = id(arr)
+
+    pool = get_reusable_pool()
+    memview = pool.add_object(python_id, arr.size * arr.itemsize)
+
+    out = ndarrayproxy(arr.shape, dtype=arr.dtype, buffer=memview)
+    out.python_id = python_id
+    out[:] = arr[:]
+
+    weakref.finalize(out, pool.detach_object, python_id)
+    return out
+
+
+def get_reusable_pool() -> ShmObjectPool:
+    global _GLOBAL_POOL
+
+    if _GLOBAL_POOL is None:
+        raise PoolNotRunning()
+
+    return _GLOBAL_POOL
+
+
+@contextmanager
+def object_pool(
+    *,
+    slot_count: int = 10_000,
+    data_size: MemorySizeType = "512M",
+    link_path: Optional[Union[Path, str]] = None,
+) -> Iterator[None]:
+    global _GLOBAL_POOL
+
+    path = link_path or f"{tempfile.gettempdir()}/pyarraypool.seg"
+
+    if _GLOBAL_POOL is not None:
+        raise PoolAlreadyExists()
+
+    _GLOBAL_POOL = ShmObjectPool(
+        slot_count=slot_count,
+        data_size=_parse_datasize_to_bytes(data_size),
+        path=str(path),
+    )
+    LOGGER.info("Pool attached (data_size: %s)", data_size)
+    try:
+        yield
+    finally:
+        _GLOBAL_POOL = None
+        LOGGER.info("Pool detached")
---
+++ tests/test_pool.py
@@ -0,0 +1,162 @@
+import pickle
+import tempfile
+from uuid import uuid1
+import pyarraypool
+import pytest
+
+import numpy as np
+
+
+class TestParseDataSize:
+    @pytest.mark.parametrize("value,expected", [
+        ("1G", 1024 ** 3),
+        ("2.4G", int(2.4 * 1024 ** 3)),
+        ("5M", 5 * 1024 ** 2),
+        ("1K", 1024),
+        ("1.5K", 1.5 * 1024),
+        (1.5, 1),
+        (256, 256),
+    ])
+    def test_valid(self, value: pyarraypool.MemorySizeType, expected: int) -> None:
+        assert pyarraypool._parse_datasize_to_bytes(value) == expected
+
+    @pytest.mark.parametrize("value", [
+        -5,
+        0,
+        "0",
+        "0.8",
+        "0G",
+    ])
+    def test_null_value(self, value: pyarraypool.MemorySizeType) -> None:
+        with pytest.raises(ValueError, match="pool size cannot be null or less"):
+            pyarraypool._parse_datasize_to_bytes(value)
+
+    @pytest.mark.parametrize("value", [
+        "Awesome",
+        "FewG",
+    ])
+    def test_invalid_value(self, value: pyarraypool.MemorySizeType) -> None:
+        with pytest.raises(ValueError):
+            pyarraypool._parse_datasize_to_bytes(value)
+
+
+@pytest.fixture
+def link_path() -> str:
+    return f"{tempfile.gettempdir()}/test_pool_{uuid1()}.seg"
+
+
+def assert_pool_off() -> None:
+    with pytest.raises(pyarraypool.PoolNotRunning):
+        pyarraypool.get_reusable_pool()
+
+
+def assert_pool_on() -> None:
+    assert pyarraypool.get_reusable_pool() is not None
+
+
+class TestPoolLifecycle:
+    def test_basic(self, link_path: str) -> None:
+        # Pool off
+        assert_pool_off()
+
+        with pyarraypool.object_pool(link_path=link_path):
+            # Pool running
+            assert_pool_on()
+
+        # Pool stopped
+        assert_pool_off()
+
+    def test_init_twice(self, link_path: str) -> None:
+        assert_pool_off()
+
+        with pyarraypool.object_pool(link_path=link_path):
+            assert_pool_on()
+
+            # Check error is raised
+            with pytest.raises(pyarraypool.PoolAlreadyExists):
+                with pyarraypool.object_pool(link_path=link_path):
+                    ...
+
+            # Check if pool is still running
+            assert_pool_on()
+
+        assert_pool_off()
+
+    def test_init_and_release_multiple(self, link_path: str) -> None:
+        assert_pool_off()
+
+        with pyarraypool.object_pool(link_path=link_path):
+            assert_pool_on()
+        assert_pool_off()
+
+        with pyarraypool.object_pool(link_path=link_path):
+            assert_pool_on()
+        assert_pool_off()
+
+        with pyarraypool.object_pool(link_path=link_path):
+            assert_pool_on()
+        assert_pool_off()
+
+    def test_attach_not_running(self, link_path: str) -> None:
+        with pytest.raises(pyarraypool.PoolNotRunning):
+            pyarraypool.get_reusable_pool()
+
+    def test_clear_if_crash_and_attach(self, link_path: str) -> None:
+        assert_pool_off()
+
+        with pytest.raises(ValueError):
+            with pyarraypool.object_pool(link_path=link_path):
+                assert_pool_on()
+                raise ValueError(":(")
+
+        assert_pool_off()
+
+
+class TestArrayProxy:
+    @pytest.fixture(autouse=True)
+    def shm_ctx(self, link_path):
+        with pyarraypool.object_pool(link_path=link_path):
+            yield
+
+    def test_has_python_id(self):
+        arr = np.arange(5)
+        proxy = pyarraypool.make_transferable(arr)
+        assert proxy.python_id != 0
+
+    def test_getattr(self):
+        arr = np.arange(5)
+        proxy = pyarraypool.make_transferable(arr)
+        assert arr.min() == proxy.min() == 0
+        assert arr.max() == proxy.max() == 4
+
+    def test_get_item(self):
+        arr = np.arange(5)
+        proxy = pyarraypool.make_transferable(arr)
+        assert arr[0] == proxy[0]
+        assert (arr == proxy).all()
+
+    def test_other_attributes(self):
+        arr = np.arange(5)
+        proxy = pyarraypool.make_transferable(arr)
+        assert len(arr) == len(proxy)
+
+    def test_multiple_dimensions(self):
+        arr = np.arange(10).reshape(2, 5)
+        proxy = pyarraypool.make_transferable(arr)
+        assert proxy.shape == (2, 5)
+        assert (arr == proxy).all()
+
+    def test_pickling(self):
+        arr = np.arange(1000)
+        proxy = pyarraypool.make_transferable(arr)
+
+        raw_arr = pickle.dumps(arr)
+        raw_proxy = pickle.dumps(proxy)
+        assert len(raw_proxy) < len(raw_arr)
+
+        arr2 = pickle.loads(raw_arr)
+        proxy2 = pickle.loads(raw_proxy)
+
+        assert (arr == arr2).all()
+        assert (proxy == proxy2).all()
+        assert (arr == proxy2).all()
