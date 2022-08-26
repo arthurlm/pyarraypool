fec39ad3d20e4388d1e3c3280373bb4f337ae37f
Arthur LE MOIGNE <arthur.lemoigne@gmail.com>
2022-08-26 10:22:36 +0200

    flake8 on pyi files

--- python/pyarraypool/pyarraypool.pyi
+++ python/pyarraypool/pyarraypool.pyi
@@ -1,26 +1,27 @@
-from typing import Optional
-
-
 class ShmObjectPool:
     def __init__(
         self, *,
         slot_count: int = 5000,
         data_size: int = 524288000,
         path: str = "pyarraypool.seg",
-    ) -> None: ...
+    ) -> None:
+        ...
 
     def add_object(
         self,
         python_id: int,
         request_size: int,
-    ) -> memoryview: ...
+    ) -> memoryview:
+        ...
 
     def attach_object(
         self,
         python_id: int,
-    ) -> memoryview: ...
+    ) -> memoryview:
+        ...
 
     def detach_object(
         self,
         python_id: int,
-    ) -> memoryview: ...
+    ) -> None:
+        ...
